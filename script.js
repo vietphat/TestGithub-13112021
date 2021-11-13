7b91f2fecff9b126133dac2d130ec991e3436c02
phatle199 <vietphatt1909@gmail.com>
2021-11-13 20:43:16 +0700

    them-file-script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector(".icon-search").addEventListener("click", () => {
+    document.querySelector("div.container-search").classList.toggle("show");
+    document.querySelector("div.header").classList.toggle("hide");
+  });
